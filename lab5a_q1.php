e8c02da918caefea557c8753b91ee1fe1f5b2de3
JasonRaj <jasonrajedward@gmail.com>
2024-11-25 16:18:01 +0800

    test

---
+++ lab5a_q1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 10px auto;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #ADD8E6;
+        }
+    </style>
+</head>
+
+<body>
+    <?php 
+        $name = "Jason Raj A/L Edward";
+        $matricNumber = "DI220082";
+        $course = "Bachelor of Computer Science (Information Security)";
+        $yearOfStudy = "Year 3";
+        $address = "No.31, Kledang Hill 21, Taman Kledang Hill, 30100 Ipoh, Perak";
+    ?>
+
+    <table>
+        <tr>
+            <th>Field</th>
+            <th>Details</th>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo $matricNumber; ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo $course; ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo $yearOfStudy; ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $address; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ lab5a_q3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q3</title>
+</head>
+
+<body>
+    <?php
+        function calculateArea($width, $height) 
+        {
+            return $width * $height;
+        }
+
+        $width = 4;
+        $height = 2;
+
+        $area = calculateArea($width, $height);
+
+        echo "<b>The area of a rectangle with a width of $width and a height of $height is $area.<b>";
+    ?>
+</body>
+</html>
